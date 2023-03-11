1ca6f679fc38b7f5b99d8546ba473da37a475917
Maria Sevil <sevil1505@gmail.com>
2023-03-11 22:31:42 +0300

    Task 4_2HW

---
+++ Lesson_4/HW/4_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine( "Enter a number:  ");
+
+int SumNum(int num)
+{
+    int sum = 0;
+    for(int i = num; num != 0; num/=10){
+      
+      sum += num % 10;
+    }
+    return sum;      
+}
+ 
+ int num = int.Parse(Console.ReadLine()!);
+ 
+ Console.WriteLine(SumNum(num));
+
