c9fba8bf04427f40db626495a83330bcf7babe54
MariaSevil <125469456+MariaSevil@users.noreply.github.com>
2023-03-31 20:24:53 +0300

    Task 7_1 hw

---
+++ Lesson_7/HW/7_1/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+
+
+void Print(double[,] arr)
+{
+
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+double[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
+
+
