11e1285b570c1db007b610a69345b318b4ecc31b
MariaSevil <125469456+MariaSevil@users.noreply.github.com>
2023-04-01 10:26:26 +0300

    Task 7_2 hw

---
+++ Lesson_7/HW/7_2/Program.cs
@@ -0,0 +1,62 @@
+﻿// : Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание, что такого элемента нет
+
+//Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+void Print(int[,] arr)
+{
+
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+void SearchArr(int[,] array)
+{
+    int i, j;
+    Random rand = new Random();
+    for (i = 0; i < array.GetLength(0); i++)
+    {
+        for (j = 0; j < array.GetLength(1); j++)
+    
+        {
+            if (i < 1 || j < 1)
+                Console.WriteLine(" ");
+        
+else
+
+    System.Console.WriteLine();
+}
+    } 
+}
+    
+
+    int num_row = int.Parse(Console.ReadLine()!);
+    int num_column = int.Parse(Console.ReadLine()!);
+    int start = int.Parse(Console.ReadLine()!);
+    int stop = int.Parse(Console.ReadLine()!);
+
+    int[,] mass = MassNums(num_row, num_column, start, stop);
+    Print(mass);
+ 
---
+++ Lesson_7/HW/7_3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+  
+  void Print(int[,] arr)
+{
+
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+     for (int i = 0; i < row; i++)
+     {
+    for (int j = 0; j < column; j++)
+        Console.Write($" {arr[i,j]} ");
+         Console.WriteLine();
+     }
+    Console.WriteLine();
+}
+int[,] MassNums(int row, int column, int from, int to)
+{
+   int[,] arr = new int[row, column];
+    
+    for (int i = 0; i < row; i++)
+    for (int j = 0; j < column; j++)
+        arr[i,j] = new Random().Next(from, to);
+
+    return arr;
+}
+  Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("------------------");
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
+int num_row = int.Parse(Console.ReadLine()!);
+    int num_column = int.Parse(Console.ReadLine()!);
+    int start = int.Parse(Console.ReadLine()!);
+    int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row,num_column, start, stop);
+    Print(mass);
