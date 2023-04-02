b4faa4e67b70fe94ccb67350c7dc12b7f9022ad5
Maria Sevil <sevil1505@gmail.com>
2023-04-02 14:06:10 +0300

    Task 9_1

---
+++ Lesson_9/9_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Hello, World!");
+
+void VallueSet( int m, int n)
+{
+    if (n < m) return;
+    VallueSet(m, n -1);
+    Console.Write($" {n}" );
+}
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+VallueSet(num_1, num_2);
