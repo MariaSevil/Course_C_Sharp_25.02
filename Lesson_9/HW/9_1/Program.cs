d8ce812679bc17c44695449a4914113e3d72ffe1
Maria Sevil <sevil1505@gmail.com>
2023-04-02 15:05:49 +0300

    Task 9_1hw

---
+++ Lesson_9/HW/9_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+Console.WriteLine("Hello, World!");
+
+void VallueSet(int m, int n)
+{
+    if (m < n) return;
+     if (m % 2 == 0);
+       VallueSet(m-1, n);
+    Console.Write($" {m}");
+    
+    
+}
+int num= int.Parse(Console.ReadLine()!);
+int num_1 = int.Parse(Console.ReadLine()!);
+VallueSet(num, num_1);
