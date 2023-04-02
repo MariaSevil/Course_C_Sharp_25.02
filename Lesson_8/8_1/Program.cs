3888cb52037e77b235a6f7458fa3819b2e3737ae
Maria Sevil <sevil1505@gmail.com>
2023-04-02 10:43:08 +0300

    Task 8_2

--- Lesson_8/8_1/Program.cs
+++ Lesson_8/8_1/Program.cs
@@ -32,16 +32,17 @@ void FirstWithLast(int[,] arr)
     int row = arr.GetLength(0);
     int column = arr.GetLength(1);
 if( row != column)
-{System.Console.WriteLine(" ");
+{
+    Console.WriteLine(" ");
 return;
 }
     for (int i = 0; i < row; i++)
     for(int j = 0; j < i ; j++)
         (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
 }
-System.Console.WriteLine(" Enter the number of rows: ");
+Console.WriteLine(" Enter the number of rows: ");
 int num_row = int.Parse(Console.ReadLine()!);
-System.Console.WriteLine("Enter the number of columns:");
+Console.WriteLine("Enter the number of columns:");
 int num_column = int.Parse(Console.ReadLine()!);
 
 int start = int.Parse(Console.ReadLine()!);
