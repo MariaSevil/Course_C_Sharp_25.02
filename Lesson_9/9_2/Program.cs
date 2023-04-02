f2673a862913df237408570a138d90f67fab30e0
Maria Sevil <sevil1505@gmail.com>
2023-04-02 14:18:17 +0300

    Task 9_2

---
+++ Lesson_9/9_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. Использовать рекурсию.
+
+Console.WriteLine("Hello, World!");
+
+int VallueSet( int N)
+{
+    if (N == 0) return 0;
+    return VallueSet (N / 10) + N % 10;
+   
+}
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(VallueSet(num));
