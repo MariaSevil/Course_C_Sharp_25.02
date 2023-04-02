0b4c8fd050245d2641d5522722a1ca966131af34
Maria Sevil <sevil1505@gmail.com>
2023-04-02 13:46:49 +0300

    Task 9_0

---
+++ Lesson_9/9_0/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+
+Console.WriteLine();
+
+void VallueSet(int N)
+{
+    if (N == 0) return;
+    VallueSet(N -1);
+    Console.Write($" {N}" );
+}
+int num = int.Parse(Console.ReadLine()!);
+VallueSet(num);
